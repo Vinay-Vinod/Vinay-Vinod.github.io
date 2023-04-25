1861df72883dce1a71fef5177342c1d3579a214a
Vinay Vinod <briarvinay@gmail.com>
2023-04-24 22:14:25 -0700

    finished lab8

---
+++ labs/lab8/script.js
@@ -0,0 +1,8 @@
+function toggleTheme() {
+    
+    document.body.classList.toggle("dark-mode")
+}
+
+let buttonVariable = document.getElementById("toggleButton")
+buttonVariable.onclick = toggleTheme;
+
