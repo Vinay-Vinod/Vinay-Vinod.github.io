da535c8f73b772e1afb3a523f50e6c6bf2459adc
Vinay Vinod <briarvinay@gmail.com>
2023-04-26 01:16:01 -0700

    final

--- final/script.js
+++ final/script.js
@@ -72,13 +72,90 @@ for (let i = 0; i < svgElements.length; i++) {
 }
 
 function goToNewPage() {
+  //Barbell
   if (chosenContaier == barbellContainer && chosenSVG == chest) {
     window.location.href = "barbellChest.html";
   }
 
+  if (chosenContaier == barbellContainer && (chosenSVG == leftShoulder || chosenSVG == rightShoulder)) {
+    window.location.href = "barbellShoulder.html";
+  }
+
+  if (chosenContaier == barbellContainer && (chosenSVG == rightArm || chosenSVG == leftArm)) {
+    window.location.href = "barbellArm.html";
+  }
+
+  if (chosenContaier == barbellContainer && (chosenSVG == rightLeg || chosenSVG == leftLeg)) {
+    window.location.href = "barbellLeg.html";
+  }
+
+  if (chosenContaier == barbellContainer && (chosenSVG == stomach)) {
+    window.location.href = "barbellStomach.html";
+  }
+
+  //Cable
   if (chosenContaier == cableContainer && chosenSVG == chest) {
     window.location.href = "cableChest.html";
   }
+
+  if (chosenContaier == cableContainer && (chosenSVG == leftShoulder || chosenSVG == rightShoulder)) {
+    window.location.href = "cableShoulder.html";
+  }
+
+  if (chosenContaier == cableContainer && (chosenSVG == rightArm || chosenSVG == leftArm)) {
+    window.location.href = "cableArm.html";
+  }
+
+  if (chosenContaier == cableContainer && (chosenSVG == rightLeg || chosenSVG == leftLeg)) {
+    window.location.href = "cableLeg.html";
+  }
+
+  if (chosenContaier == cableContainer && (chosenSVG == stomach)) {
+    window.location.href = "cableStomach.html";
+  }
+  
+
+ //Dumbell
+  if (chosenContaier == dumbellContainer && chosenSVG == chest) {
+    window.location.href = "dumbellChest.html";
+  }
+
+  if (chosenContaier == dumbellContainer && (chosenSVG == leftShoulder || chosenSVG == rightShoulder)) {
+    window.location.href = "dumbellShoulder.html";
+  }
+
+  if (chosenContaier == dumbellContainer && (chosenSVG == rightArm || chosenSVG == leftArm)) {
+    window.location.href = "dumbellArm.html";
+  }
+
+  if (chosenContaier == dumbellContainer && (chosenSVG == rightLeg || chosenSVG == leftLeg)) {
+    window.location.href = "dumbellLeg.html";
+  }
+
+  if (chosenContaier == dumbellContainer && (chosenSVG == stomach)) {
+    window.location.href = "dumbellStomach.html";
+  }
+
+  //BW
+  if (chosenContaier == bwContainer && chosenSVG == chest) {
+    window.location.href = "bwChest.html";
+  }
+  if (chosenContaier == bwContainer && (chosenSVG == leftShoulder || chosenSVG == rightShoulder)) {
+    window.location.href = "bwShoulder.html";
+  }
+
+  if (chosenContaier == bwContainer && (chosenSVG == rightArm || chosenSVG == leftArm)) {
+    window.location.href = "bwArm.html";
+  }
+
+  if (chosenContaier == bwContainer && (chosenSVG == rightLeg || chosenSVG == leftLeg)) {
+    window.location.href = "bwLeg.html";
+  }
+
+  if (chosenContaier == bwContainer && (chosenSVG == stomach)) {
+    window.location.href = "bwStomach.html";
+  }
+
     
 }
 
